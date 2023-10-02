891bcd13f0c387c3709c5f793df84ad0734c46b0
emmatassone <emmanueltassone@gmail.com>
2023-10-02 20:35:29 -0300

    Changes to be committed: 	new file:   trading_strategies/__pycache__/data_processing.cpython-310.pyc 	new file:   trading_strategies/__pycache__/strategies.cpython-310.pyc 	modified:   trading_strategies/data_processing.py 	modified:   trading_strategies/run_backtest.py 	modified:   trading_strategies/strategies.py

--- trading_strategies/run_backtest.py
+++ trading_strategies/run_backtest.py
@@ -3,8 +3,9 @@ import vectorbt as vbt
 import numpy as np
 from strategies import StrategyBook
 from data_processing import load_data
+import pandas as pd 
 
-ohlcv_wbuf=load_data()
+ohlcv_wbuf = load_data()
 ohlcv_wbuf = ohlcv_wbuf.astype(np.float64)
 
 
@@ -12,7 +13,7 @@ vbt.settings.portfolio['init_cash'] = 100.  # 100$
 vbt.settings.portfolio['fees'] = 0.0025  # 0.25%
 
 # Apply the strategy to generate signals
-entries,exits = StrategyBook.threshold_strategy(ohlcv_wbuf['Close'].values)
+entries,exits = StrategyBook.moving_average(ohlcv_wbuf['Close'].values)
 
 df=pd.concat([ohlcv_wbuf, pd.DataFrame(entries,index=ohlcv_wbuf.index,columns=['entry'])], axis=1)
 df=pd.concat([df, pd.DataFrame(exits,index=ohlcv_wbuf.index,columns=['exit'])], axis=1)
--- trading_strategies/strategies.py
+++ trading_strategies/strategies.py
@@ -1,9 +1,11 @@
+import vectorbt as vbt
+
 class StrategyBook:
     def __init__(self, params):
         pass
 
     @classmethod
-    def threshold_strategy(cls,price_data):
+    def threshold(cls,price_data):
         buy_threshold= 7000
         sell_threshold= 16000
         entry_signals = []
@@ -21,4 +23,12 @@ class StrategyBook:
                 exit_signals.append(False)
 
         return entry_signals, exit_signals
+    
+    @classmethod
+    def moving_average(cls,price_data,ma1=10,ma2=20):
+        fast_ma = vbt.MA.run(price_data, ma1, short_name='fast')
+        slow_ma = vbt.MA.run(price_data, ma2, short_name='slow')
+        entries = fast_ma.ma_crossed_above(slow_ma)
+        exits = fast_ma.ma_crossed_below(slow_ma)
+        return entries.values, exits.values
     
