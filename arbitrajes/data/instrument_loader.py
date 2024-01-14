63cf42644035854c28e81929ffbcdd3413927828
gmancuso01 <132796402+gmancuso01@users.noreply.github.com>
2024-01-14 05:41:46 +0000

    Creating instrument_loader.py
    This script will read the bid and ask prices of all the Argentine shares and CEDEARs.

---
+++ arbitrajes/data/instrument_loader.py
@@ -0,0 +1,13 @@
+# Importamos todo lo necesario acá
+#
+# import ...
+# import ...
+# from ... import ...
+
+# Leemos el precio del activo (acciones y CEDEARs, en ppio) a 48hs y en CI usando Matriz
+#
+# A modo de ejemplo, usamos los precios de cierre de AAPL a 48 hs y en CI
+# bid_AAPL_48 = 21142.0
+# ask_AAPL_CI = 20786.5
+
+# Definimo la función acá
