4218594d6c8426c6b039e1f294dbde3a163653a1
gmancuso01 <132796402+gmancuso01@users.noreply.github.com>
2024-01-14 17:25:10 +0000

    Create find_opportunity.py
    Clase que busca las oportunidades de arbitraje

---
+++ arbitrajes/opportunity_finder/find_opportunity.py
@@ -0,0 +1,23 @@
+
+class ArbitrageOpportunityFinder:
+    def __init__(self, bid_AAPL_48, ask_AAPL_CI, volume_CI, commi, IVA):
+        self.bid_AAPL_48 = bid_AAPL_48
+        self.ask_AAPL_CI = ask_AAPL_CI
+        self.volume_CI = volume_CI
+        self.commi = commi
+        self.IVA = IVA
+
+    def find_opportunity(self):
+        charge_buy = self.ask_AAPL_CI * (self.commi + self.IVA) / 100
+        buy_price = self.bid_AAPL_48 + charge_buy
+
+        charge_sell = self.bid_AAPL_48 * (self.commi + self.IVA) / 100
+        net_income = self.bid_AAPL_48 - charge_sell
+
+        if net_income > buy_price:
+            print("\nHay una nueva oportunidad de arbitraje!")
+            percentage_earn = (net_income - buy_price) * 100 / net_income
+            return round(percentage_earn, 2), self.volume_CI
+        else:
+            return None
+
